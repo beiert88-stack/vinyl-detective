1505b716c0d42e6bde4db2e0b29f531772271669
beiert88-stack <beiert88@gmail.com>
2026-09-02 21:03:48 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,2 +0,0 @@
-self.addEventListener("install",e=>e.waitUntil(caches.open("vinyl-detective-v1").then(c=>c.addAll(["/","/manifest.webmanifest"]))));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
